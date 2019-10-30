d3ba1e0747e695f83486e14b937c90d4340287c3
allison cortez <allizetorc@gmail.com>
2019-10-30 19:53:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+class Dog 
+  
+  def initialize(name)
+    @name = name
+    
+    
+    @@all << self
+  end 
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    dog_name= []
+    @@all.each do |dog|
+      
+    end
+    dog_name
+  end
+  
+end
