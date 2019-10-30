16e00ba09a5db14dbc926722397306e0e33c6f52
allison cortez <allizetorc@gmail.com>
2019-10-30 21:23:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,27 +5,33 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    self.save
   end 
   
   @@all = []
   
+  def save
+    @@all << self
+  end
+  
+  
+  
   def self.all
     @@all
   end
-  
   
   def self.clear_all
     @@all.clear
   end
   
   def self.print_all
-    
     # iterates over all of the individual dogs stored in the @@all array
     @@all.each do |dog, name|
     puts dog.name
     end
   end
+ 
+ 
  
   
 end
