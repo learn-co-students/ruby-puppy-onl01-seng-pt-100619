9c675676633be2feef728e2107f2494a26ab9f8b
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-31 02:10:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name, :save
+ 
+  def initialize(name)
+    @save = save
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each do |element|
+      puts "#{element.name}"
+    end
+  end
+
+ 
+end
